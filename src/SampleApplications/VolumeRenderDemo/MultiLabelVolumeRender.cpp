d3ad12ccbb8e8a5b37b757beaaaad7d120ff0b1f
peter karasev <karasevpa@gmail.com>
2011-06-21 15:27:27 -0400

    push upstream stuff from assembla, minus data

--- src/SampleApplications/VolumeRenderDemo/MultiLabelVolumeRender.cpp
+++ src/SampleApplications/VolumeRenderDemo/MultiLabelVolumeRender.cpp
@@ -14,25 +14,95 @@
 #include "vtkSmartPointer.h"
 #include "vtkMetaImageReader.h"
 #include "vtkImageResample.h"
+#include "opencv2/core/core.hpp"
 #include <vtkMath.h>
 #include <cmath>
 #include <vector>
+#include "KSegmentor.h"
 
 #define SP( X )  vtkSmartPointer<X> 
+using namespace std;
+using namespace cv;
+
+void get_good_color( int idx, vector<int>& rgb_out )
+{
+  int rgb[3]; 
+  switch( idx ) {
+   
+  case 0:
+    rgb = {255, 146, 5}; break;
+    //Dk. Gray
+  case 1:
+    rgb = {87, 87, 87}; break; 
+    //Red
+  case 2:
+    rgb = {173, 35, 35}; break;
+    //Blue
+  case 3: 
+    rgb = {42, 75, 215}; break;
+    //Green
+  case 4: 
+    rgb = {29, 105, 20}; break;
+    //Brown
+  case 5: 
+    rgb = {129, 74, 25}; break;
+    //Purple
+  case 6:
+    rgb = {129, 38, 192}; break;
+    //Lt. Gray
+    
+  case 7:
+    rgb = {160, 160, 160}; break;
+    //Lt. Green
+    
+  case 8:
+    rgb = {129, 197, 122}; break;
+    //Lt. Blue
+    
+  case 9:
+    rgb = {157, 175, 255}; break;
+    //Cyan
+    
+  case 10: 
+    rgb = {41, 208, 208}; break;
+    //Orange
+  case 11:
+    rgb = { 255, 146, 51}; break;
+    //Yellow
+  case 12: 
+    rgb = {255, 238, 51}; break;
+    //Tan
+  case 13:
+    rgb = {233, 222, 187}; break;
+    //Pink
+  case 14: 
+    rgb = {255, 205, 243}; break;
+    //White
+  case 15: 
+    rgb = {255, 255, 255}; break;
+  }
+  
+  rgb_out = std::vector<int>(rgb,rgb+3);
+  
+}
+
 
 SP(vtkImageData) image2ushort( vtkImageData* imageData )
 {
   SP(vtkImageData) imgvol = SP(vtkImageData)::New( );
   int dims[3];
   imageData->GetDimensions( dims );
+  double spacing_in[3];
+  imageData->GetSpacing(spacing_in);
+  cout << "spacing:" << Mat( vector<double>(spacing_in,spacing_in+3) ) << endl;
   
   imgvol->SetDimensions( dims[0],dims[1],dims[2] );
   imgvol->SetNumberOfScalarComponents(1);
-  imgvol->SetSpacing( 1,1,1 );
+  imgvol->SetSpacing( spacing_in );
   imgvol->SetOrigin( 0,0,0 );
   imgvol->SetScalarTypeToUnsignedShort( );
   imgvol->AllocateScalars( );
-
+  
   // Values stored 'linearly', slightly unsure about the orientation though.
   unsigned short*  outputPtr = (unsigned short *) imgvol->GetScalarPointer();
   short *inputPtr = static_cast<short*>( imageData->GetScalarPointer() );
@@ -44,7 +114,7 @@ SP(vtkImageData) image2ushort( vtkImageData* imageData )
     *outputPtr= nextvalue;
     *outputPtr++;
   }
-
+  
   return imgvol;
 }
 
@@ -57,8 +127,16 @@ SP(vtkImageData) mergeLabelMaps( const std::vector<SP(vtkImageData)> & multiLabe
   double labelRange[2];
   for( int k = 0; k < nLabels; k++ ) {
     vtkImageData* imgData = multiLabels[k];
+    
+    // display the volume of each label for quick sanity checking of usefulness of label maps
+    double spc[3];
+    imgData->GetSpacing(spc);
+    vector<double> spacing(spc,spc+3);
+    string strVolume;
+    vrcl::getVolumeAsString(spacing,imgData,strVolume);
+    cout << strVolume << endl;
     imgData->GetDimensions( dims );
-   
+    
     unsigned short*  img_k_in = (unsigned short *) imgData->GetScalarPointer();
     unsigned short*  img_out  = (unsigned short *) img->GetScalarPointer();
     int numel                 = dims[0]*dims[1]*dims[2];
@@ -77,11 +155,11 @@ SP(vtkImageData) mergeLabelMaps( const std::vector<SP(vtkImageData)> & multiLabe
 
 int main( int argc, char **argv)
 {
- SP(vtkMetaImageReader) imgReader =  SP(vtkMetaImageReader)::New();
- SP(vtkMetaImageReader) lblReader =  SP(vtkMetaImageReader)::New();
- 
- std::vector< SP(vtkImageData) > multiLabels(argc-1);
- 
+  SP(vtkMetaImageReader) imgReader =  SP(vtkMetaImageReader)::New();
+  SP(vtkMetaImageReader) lblReader =  SP(vtkMetaImageReader)::New();
+  
+  std::vector< SP(vtkImageData) > multiLabels(argc-1);
+  
   if( argc >= 2 ) {
     for( int k = 1; k < argc; k++ ) {
       cout << "attempting to load " << argv[k] << endl;
@@ -107,65 +185,61 @@ int main( int argc, char **argv)
   resampler->SetInput( imageData );
   resampler->SetAxisMagnificationFactor(0,1.0);
   resampler->SetAxisMagnificationFactor(1,1.0);
-  resampler->SetAxisMagnificationFactor(2,15.0);
+  resampler->SetAxisMagnificationFactor(2,1.0);
   resampler->Update();
   imageData = resampler->GetOutput();
-  
-//  vtkIndent indentVTK=vtkIndent::vtkIndent(0);
-//  std::ofstream  LogFile("kviewer_desktop.log");  
-//  imageData->PrintSelf(LogFile, indentVTK);
-//  imageData->PrintSelf(std::cout, indentVTK);
-  
+    
   double imgRange[2];
   imageData->GetScalarRange( imgRange );
   
   vtkPiecewiseFunction *opacityTranferFunction = vtkPiecewiseFunction::New();
-
+  
   opacityTranferFunction->AddPoint( imgRange[1],     0.8);
   opacityTranferFunction->AddPoint( 2,0.8);
   opacityTranferFunction->AddPoint( 1,0.0);
   opacityTranferFunction->AddPoint( 0,     0.0);
   SP(vtkColorTransferFunction) colorTransferFunction = SP(vtkColorTransferFunction)::New();
   std::vector<double> v(2*multiLabels.size());
+  std::vector<int> rgb(3);
   for( int k = 0; k < (int) v.size(); k++ ) {
     v[k] =  (k/double(v.size())) * imgRange[1] + (1.0 - k/double(v.size())) * imgRange[0];
     cout << "added colorpoint ... " << v[k] << endl;
-    colorTransferFunction->AddRGBPoint(   v[k], k/double(v.size()), vtkMath::Random(0,1), vtkMath::Random(0,1) );
+    get_good_color(k,rgb);
+    colorTransferFunction->AddRGBPoint(   v[k], rgb[0]/255.0, rgb[1]/255.0, rgb[2]/255.0 );
   }
   
   //
   SP(vtkVolumeProperty) volumeProperty = SP(vtkVolumeProperty)::New();
   volumeProperty->SetColor(colorTransferFunction);
-  //volumeProperty->SetGradientOpacity( opacityTranferFunction );
   volumeProperty->SetScalarOpacity(opacityTranferFunction);
   volumeProperty->ShadeOn();
   volumeProperty->SetInterpolationTypeToLinear();
-
+  
   //
   SP(vtkVolumeRayCastCompositeFunction) compositeFunction = 
-               SP( vtkVolumeRayCastCompositeFunction)::New();
+      SP( vtkVolumeRayCastCompositeFunction)::New();
   SP(vtkVolumeRayCastMapper) volumeMapper = SP(vtkVolumeRayCastMapper)::New();
   volumeMapper->SetVolumeRayCastFunction(compositeFunction);
-
+  
   volumeMapper->SetInput( imageData );
-
+  
   volumeMapper->Update( );
-
+  
   //
   vtkVolume *volume = vtkVolume::New();
   volume->SetMapper(volumeMapper);
   volume->SetProperty(volumeProperty);
-
+  
   vtkRenderer *ren1 = vtkRenderer::New();
   vtkRenderWindow *renWin = vtkRenderWindow::New();
   ren1->SetBackground(1.0,1.0,1.0);
   renWin->AddRenderer(ren1);
   vtkRenderWindowInteractor *renderWindowInteractor = vtkRenderWindowInteractor ::New();
   renderWindowInteractor->SetRenderWindow(renWin);
-
+  
   ren1->AddVolume(volume);
   ren1->Render();
   renderWindowInteractor->Start();
-
+  
   return 0;
 }
