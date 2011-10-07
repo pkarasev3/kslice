639e50db04c8b0d81029f8f3becda46b291295b5
pkarasev <pkarasev@gatech.edu>
2011-10-07 15:28:49 -0400

    update data script

--- data/get_data_from_web_download.sh
+++ data/get_data_from_web_download.sh
@@ -1,1 +1,11 @@
 scp tsfat:/u4/public/karasev/data_medical/kslice_data/*.mha  ./ 
+
+# Email peter karasev and/or ivan kolesov regarding data samples ... 
+# your random MHA files should work too but there might be scaling/shifting issues! 
+
+# In particular, CT might be trickier due to different range...
+#  fixes are welcome and highly appreciated! 
+
+
+# karasevpa@gmail.com
+# ivan.kolesov@gmail.com
