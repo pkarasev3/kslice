f14f270cfc74e7babaad4a30b77662f05993ab13
Ivan Kolesov <ivan.kolesov@gatech.edu>
2013-02-22 18:47:46 -0500

    exp

--- src/kslice_wrap/KSliceEffect.py
+++ src/kslice_wrap/KSliceEffect.py
@@ -5,6 +5,7 @@ from EditorLib.EditOptions import HelpButton
 from EditorLib.EditOptions import EditOptions
 from EditorLib import EditUtil
 from EditorLib import LabelEffect
+#import ../qt-loadable-modules/vtkSlicerKSliceModuleLogicPython
 
 #
 # The Editor Extension itself.
@@ -71,6 +72,11 @@ class KSliceEffectOptions(EditorLib.LabelEffectOptions):
 
   def onApply(self):
     print('This is just an example - nothing here yet')
+    
+    #logic=vtkSlicerKSliceModuleLogicPython.vtkKSlice() #IKDebug
+    #print(logic)
+    #logic.PrintEmpty()
+
 
   def updateMRMLFromGUI(self):
     if self.updatingGUI:
