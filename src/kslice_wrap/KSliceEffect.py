9c89dbe3b7ceff19f0b7bb1351cc7c5f8e625dc1
Ivan Kolesov <ivan.kolesov@gatech.edu>
2013-02-22 18:11:53 -0500

    added editor effect, start filling it in ...

---
+++ src/kslice_wrap/KSliceEffect.py
@@ -0,0 +1,227 @@
+import os
+from __main__ import vtk, qt, ctk, slicer
+import EditorLib
+from EditorLib.EditOptions import HelpButton
+from EditorLib.EditOptions import EditOptions
+from EditorLib import EditUtil
+from EditorLib import LabelEffect
+
+#
+# The Editor Extension itself.
+#
+# This needs to define the hooks to be come an editor effect.
+#
+
+#
+# KSliceEffectOptions - see LabelEffect, EditOptions and Effect for superclasses
+#
+
+class KSliceEffectOptions(EditorLib.LabelEffectOptions):
+  """ KSliceEffect-specfic gui
+  """
+
+  def __init__(self, parent=0):
+    super(KSliceEffectOptions,self).__init__(parent)
+
+    # self.attributes should be tuple of options:
+    # 'MouseTool' - grabs the cursor
+    # 'Nonmodal' - can be applied while another is active
+    # 'Disabled' - not available
+    self.attributes = ('MouseTool')
+    self.displayName = 'KSliceEffect Effect'
+
+  def __del__(self):
+    super(KSliceEffectOptions,self).__del__()
+
+  def create(self):
+    super(KSliceEffectOptions,self).create()
+    self.apply = qt.QPushButton("Apply", self.frame)
+    self.apply.setToolTip("Apply the extension operation")
+    self.frame.layout().addWidget(self.apply)
+    self.widgets.append(self.apply)
+
+    HelpButton(self.frame, "This is a sample with no real functionality.")
+
+    self.apply.connect('clicked()', self.onApply)
+
+    # Add vertical spacer
+    self.frame.layout().addStretch(1)
+
+  def destroy(self):
+    super(KSliceEffectOptions,self).destroy()
+
+  # note: this method needs to be implemented exactly as-is
+  # in each leaf subclass so that "self" in the observer
+  # is of the correct type
+  def updateParameterNode(self, caller, event):
+    node = EditUtil.EditUtil().getParameterNode()
+    if node != self.parameterNode:
+      if self.parameterNode:
+        node.RemoveObserver(self.parameterNodeTag)
+      self.parameterNode = node
+      self.parameterNodeTag = node.AddObserver(vtk.vtkCommand.ModifiedEvent, self.updateGUIFromMRML)
+
+  def setMRMLDefaults(self):
+    super(KSliceEffectOptions,self).setMRMLDefaults()
+
+  def updateGUIFromMRML(self,caller,event):
+    self.updatingGUI = True
+    super(KSliceEffectOptions,self).updateGUIFromMRML(caller,event)
+    self.updatingGUI = False
+
+  def onApply(self):
+    print('This is just an example - nothing here yet')
+
+  def updateMRMLFromGUI(self):
+    if self.updatingGUI:
+      return
+    disableState = self.parameterNode.GetDisableModifiedEvent()
+    self.parameterNode.SetDisableModifiedEvent(1)
+    super(KSliceEffectOptions,self).updateMRMLFromGUI()
+    self.parameterNode.SetDisableModifiedEvent(disableState)
+    if not disableState:
+      self.parameterNode.InvokePendingModifiedEvent()
+
+
+#
+# KSliceEffectTool
+#
+
+class KSliceEffectTool(LabelEffect.LabelEffectTool):
+  """
+  One instance of this will be created per-view when the effect
+  is selected.  It is responsible for implementing feedback and
+  label map changes in response to user input.
+  This class observes the editor parameter node to configure itself
+  and queries the current view for background and label volume
+  nodes to operate on.
+  """
+
+  def __init__(self, sliceWidget):
+    super(KSliceEffectTool,self).__init__(sliceWidget)
+
+  def cleanup(self):
+    super(KSliceEffectTool,self).cleanup()
+
+  def processEvent(self, caller=None, event=None):
+    """
+    handle events from the render window interactor
+    """
+    if event == "LeftButtonPressEvent":
+      xy = self.interactor.GetEventPosition()
+      sliceLogic = self.sliceWidget.sliceLogic()
+      logic = KSliceEffectLogic(sliceLogic)
+      logic.apply(xy)
+      print("Got a %s at %s in %s", (event,str(xy),self.sliceWidget.sliceLogic().GetSliceNode().GetName()))
+      self.abortEvent(event)
+    else:
+      pass
+
+
+#
+# KSliceEffectLogic
+#
+
+class KSliceEffectLogic(LabelEffect.LabelEffectLogic):
+  """
+  This class contains helper methods for a given effect
+  type.  It can be instanced as needed by an KSliceEffectTool
+  or KSliceEffectOptions instance in order to compute intermediate
+  results (say, for user feedback) or to implement the final
+  segmentation editing operation.  This class is split
+  from the KSliceEffectTool so that the operations can be used
+  by other code without the need for a view context.
+  """
+
+  def __init__(self,sliceLogic):
+    self.sliceLogic = sliceLogic
+
+  def apply(self,xy):
+    pass
+
+
+#
+# The KSliceEffectExtension class definition
+#
+
+class KSliceEffectExtension(LabelEffect.LabelEffect):
+  """Organizes the Options, Tool, and Logic classes into a single instance
+  that can be managed by the EditBox
+  """
+
+  def __init__(self):
+    # name is used to define the name of the icon image resource (e.g. KSliceEffect.png)
+    self.name = "KSliceEffect"
+    # tool tip is displayed on mouse hover
+    self.toolTip = "Paint: circular paint brush for label map editing"
+
+    self.options = KSliceEffectOptions
+    self.tool = KSliceEffectTool
+    self.logic = KSliceEffectLogic
+
+""" Test:
+
+sw = slicer.app.layoutManager().sliceWidget('Red')
+import EditorLib
+pet = EditorLib.KSliceEffectTool(sw)
+
+"""
+
+#
+# KSliceEffect
+#
+
+class KSliceEffect:
+  """
+  This class is the 'hook' for slicer to detect and recognize the extension
+  as a loadable scripted module
+  """
+  def __init__(self, parent):
+    parent.title = "Editor KSliceEffect Effect"
+    parent.categories = ["Developer Tools.Editor Extensions"]
+    parent.contributors = ["Steve Pieper (Isomics)"] # insert your name in the list
+    parent.helpText = """
+    Example of an editor extension.  No module interface here, only in the Editor module
+    """
+    parent.acknowledgementText = """
+    This editor extension was developed by
+    <Author>, <Institution>
+    based on work by:
+    Steve Pieper, Isomics, Inc.
+    based on work by:
+    Jean-Christophe Fillion-Robin, Kitware Inc.
+    and was partially funded by NIH grant 3P41RR013218.
+    """
+
+    # TODO:
+    # don't show this module - it only appears in the Editor module
+    #parent.hidden = True
+
+    # Add this extension to the editor's list for discovery when the module
+    # is created.  Since this module may be discovered before the Editor itself,
+    # create the list if it doesn't already exist.
+    try:
+      slicer.modules.editorExtensions
+    except AttributeError:
+      slicer.modules.editorExtensions = {}
+    slicer.modules.editorExtensions['KSliceEffect'] = KSliceEffectExtension
+
+#
+# KSliceEffectWidget
+#
+
+class KSliceEffectWidget:
+  def __init__(self, parent = None):
+    self.parent = parent
+
+  def setup(self):
+    # don't display anything for this widget - it will be hidden anyway
+    pass
+
+  def enter(self):
+    pass
+
+  def exit(self):
+    pass
+
+
